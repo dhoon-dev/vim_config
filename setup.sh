4ee254bd71610fba6dd36e81bc46f468752d726c
Donghoon Nam <dhoon.nam@gmail.com>
2019-03-17 20:51:26 +0900

    setup.sh installs vim-plug

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
 #!/usr/bin/env sh
 
+rm "$HOME/.vimrc"
+rm -r "$HOME/.vim"
+
 cp -r vim "$HOME/.vim"
 cp vimrc "$HOME/.vimrc"
+
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
